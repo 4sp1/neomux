1af85d2a4b61c150b38b0e209d6258c3e31934f8
Malik Benkirane <dev@lafourgale.com>
2025-11-14 12:53:28 +0100

    root.dev-202511141253.28
    Add orphaned state cleanup to list command
    
    Add functionality to detect and display orphaned server labels when
    listing servers. This helps identify server entries in the state that
    no longer have corresponding processes running.
    
    - Clean state before listing servers
    - Display orphaned labels if any are found

--- cmd/list.go
+++ cmd/list.go
@@ -3,7 +3,10 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"strings"
 
+	procs "github.com/4sp1/neomux/internal/adapter/os/process"
+	"github.com/4sp1/neomux/internal/app"
 	"github.com/spf13/cobra"
 )
 
@@ -17,13 +20,40 @@ func newListCmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
+
+			proc, err := procs.New()
+			if err != nil {
+				return fmt.Errorf("procs adapter: new: %w", err)
+			}
+
+			app, err := app.New(proc, state)
+			if err != nil {
+				return fmt.Errorf("app: new: %w", err)
+			}
+
+			labels, err := app.StateClean()
+			if err != nil {
+				return fmt.Errorf("app: state clean: %w", err)
+			}
+			{
+				ls := make([]string, len(labels))
+				for i, l := range labels {
+					ls[i] = string(l)
+				}
+				if len(ls) > 0 {
+					fmt.Println("orphaned:", strings.Join(ls, ", "))
+				}
+			}
+
 			servers, err := state.ListServers(context.Background())
 			if err != nil {
 				return fmt.Errorf("list servers: %w", err)
 			}
+
 			for _, s := range servers {
 				fmt.Printf("%s:%s\n", s.Label, s.Workdir)
 			}
+
 			return nil
 		},
 	}
